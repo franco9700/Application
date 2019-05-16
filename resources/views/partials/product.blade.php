be3dba951e5cc168428dcd785ac8d204161be837
Ana Paola Mirazo R <anap_mirazo.ron@hotmail.com>
2019-05-15 20:52:55 -0700

    Making the preview of the product beatifull

---
+++ resources/views/partials/product.blade.php
@@ -0,0 +1,50 @@
+@foreach ($product_search as $product)
+<div class="card border-light mb-3" style="max-width:">
+    <div class="card-body">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h5 class="card-title">{{ $product->name }}</h5>
+                    <p class="card-text">
+                        {{$product->description}}
+                        <br>
+                        Precio: ${{ $product->price }}</p>
+                        
+                </div>
+                <div class="col" style="max-width: 10rem;">
+                    <img src="https://static.grainger.com/rp/s/is/image/Grainger/6CTF0_AS02?$zmmain$" alt="imagen de prueba" class="img-fluid">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col col-lg-10 col-md-8 col-sm-2">
+                    <button type="button" class="btn btn-warning"><link href="https://useiconic.com/open/#" rel="stylesheet">Agregar a la lista</button>
+                </div>
+            </div>
+      </div>
+    </div>
+</div>
+@endforeach
+<!--
+<table class="table table-stripped">
+    <thead>
+        <td>Name</td>
+        <td>Category</td>
+        <td>Stock</td>
+        <td>Price</td>
+        <td>Subsidiaries</td>
+    </thead>
+    <tbody>
+        
+        <tr>
+            <td></td>
+            <td>{{ $product->category->description }}</td>
+            <td>{{ $product->stock }}</td>
+            <td></td>
+            <td>{{ $product->subsidiary->name }}</td>
+
+        </tr>
+
+
+    </tbody>
+</table>
+>
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -4,42 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Results</div>
-
-                <div class="card-body">
+            
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    <table class="table table-stripped">
-                    	<thead>
-                    		<td>Name</td>
-                    		<td>Category</td>
-                    		<td>Stock</td>
-                    		<td>Price</td>
-                    		<td>Subsidiaries</td>
-                    	</thead>
-                    	<tbody>
-                    		@foreach ($product_search as $product)
-                    		<tr>
-	                    		<td>{{ $product->name }}</td>
-	                    		<td>{{ $product->category->description }}</td>
-	                    		<td>{{ $product->stock }}</td>
-	                    		<td>{{ $product->price }}</td>
-	                    		<td>{{ $product->subsidiary->name }}</td>
-
-	                    	</tr>
-
-                    		@endforeach
-                    	</tbody>
-                    </table>
-                </div>
-            </div>
+                    @include('partials.product')
+        
         </div>
     </div>
 </div>
 @endsection
-
